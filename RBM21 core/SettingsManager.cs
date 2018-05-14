03cf2f56c701bca37a68b1d04405d5e6c91b9e20
point86 <paolo.venturato@gmail.com>
2018-05-15 00:49:59 +0200

    Added custom settings class

--- RBM21 core/SettingsManager.cs
+++ RBM21 core/SettingsManager.cs
@@ -8,24 +8,54 @@ using System.Text.RegularExpressions;
 
 namespace RBM21_core
 {
+
+    /* settings file format:
+     * CameFilePath C:\\programs\\came\\cai.tsp
+     * SQLiteDB C:\\users\\desktop\\cai.sqlite
+     * Enabled true
+     */
     class SettingsManager
     {
-        string DatabasePath;
-        string CameFilePath;
-        bool active;
+        private string CameFilePath = null;
+        private string SQLiteDB = null;
+        private string enabled = null;
+        private string path = null;
 
         public SettingsManager(string path)
         {
-            StreamReader sr = File.OpenText(path);
+            this.path = path;
+            string[] lines = System.IO.File.ReadAllLines(path);
 
+            /* if input is:
+             * SQLiteDB C:\\users\\desktop\\cai.sqlite
+             * Regex.match will capture:
+             * C:\\users\\desktop\\cai.sqlite 
+             * so throw away first word, first blank characters and return all text remaining
+             */
+            CameFilePath = Regex.Match(lines[0], @"^\w+\s+(.+)").Groups[1].Value;
+            SQLiteDB = Regex.Match(lines[1], @"^\w+\s+(.+)").Groups[1].Value;
+            enabled = Regex.Match(lines[2], @"^\w+\s+(.+)").Groups[1].Value;            
         }
-
         public void SaveSettings()
         {
-
+            string[] lines = {
+                    "CameFilePath " + this.CameFilePath,
+                    "SQLiteDB " + this.SQLiteDB,
+                    "enabled "  + this.enabled
+            };
+            System.IO.File.WriteAllLines(this.path, lines);
         }
-
-
-
+        public string GetCameFilePath()
+        {
+            return CameFilePath;
+        }
+        public string GetSQLITEDB()
+        {
+            return SQLiteDB;
+        }
+        public string GetEnabled()
+        {
+            return enabled;
+        }        
     }
 }
